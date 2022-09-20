ce09c1444515b4e0b0bb6437e217c54f09ff9a04
PermabanSun <295375895@qq.com>
2022-09-20 15:18:51 +0800

    修改了返回值类型

--- src/main/java/com/hmdp/utils/UserHolder.java
+++ src/main/java/com/hmdp/utils/UserHolder.java
@@ -1,23 +1,23 @@
 package com.hmdp.utils;
 
-import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
 
 /**
  * @BelongProject: hm-dianping
- * @BelongPackage: com.hmdp.config
+ * @BelongPackage: com.hmdp.utils
  * @Author: 那个小楠瓜
  * @CreateTime: 2022-09-19 21:30
  * @Description: 用户日记评论表服务类
  * @Version: 1.0
  */
 public class UserHolder {
-    private static final ThreadLocal<UserDTO> tl = new ThreadLocal<>();
+    private static final ThreadLocal<User> tl = new ThreadLocal<>();
 
-    public static void saveUser(UserDTO user){
+    public static void saveUser(User user){
         tl.set(user);
     }
 
-    public static UserDTO getUser(){
+    public static User getUser(){
         return tl.get();
     }
 
