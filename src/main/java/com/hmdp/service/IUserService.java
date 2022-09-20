f077185f1f979fcf0310a6d2696ef4b2c3b78b48
PermabanSun <295375895@qq.com>
2022-09-20 15:20:38 +0800

    增加了发送验证码和用户登录接口

--- src/main/java/com/hmdp/service/IUserService.java
+++ src/main/java/com/hmdp/service/IUserService.java
@@ -1,11 +1,15 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * @BelongProject: hm-dianping
- * @BelongPackage: com.hmdp.config
+ * @BelongPackage: com.hmdp.service
  * @Author: 那个小楠瓜
  * @CreateTime: 2022-09-19 21:15
  * @Description: 用户表服务类
@@ -13,4 +17,7 @@ import com.hmdp.entity.User;
  */
 public interface IUserService extends IService<User> {
 
+    Result sendCode(String phone, HttpSession session);
+
+    Result login(LoginFormDTO loginForm, HttpSession session);
 }
