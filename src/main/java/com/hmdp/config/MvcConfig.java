5a70cfbf46d42e57f6cbdc98fc5644ab6f0cd2b6
PermabanSun <295375895@qq.com>
2022-09-20 15:22:08 +0800

    设置了请求过滤

---
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,31 @@
+package com.hmdp.config;
+
+import com.hmdp.utils.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @BelongProject: hm-dianping
+ * @BelongPackage: com.hmdp.config
+ * @Author: 那个小楠瓜
+ * @CreateTime: 2022-09-20 15:13
+ * @Description: mvc 配置
+ * @Version: 1.0
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "/shop/**",
+                        "/voucher/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/blog/hot",
+                        "/user/code",
+                        "/user/login"
+                );
+    }
+}
