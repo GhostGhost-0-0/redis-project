67a77d5fda92c350671b10a13d8b68c4455d0013
PermabanSun <295375895@qq.com>
2022-09-21 20:59:28 +0800

    实现了悲观锁解决一人一单的超卖线程问题

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -9,6 +9,7 @@ import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -60,18 +61,43 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             // 扣减失败
             return Result.fail("库存不足！");
         }
+
         // 6.创建订单
-        // 6.1.订单 id
+        Long userId = UserHolder.getUser().getId();
+
+        // 对每个用户进行加锁，减少悲观锁的性能影响，因为 synchronized 锁的是当前对象
+        // 每个用户就是一个对象，因此每个用户都有自己的锁
+        // 但是 toString() 方法底层实际上是重新 new 了一个 String 对象，这样会导致
+        // 即使是同一个用户每次进到这个方法依旧会拿到一把锁，跟预期不符
+        // 因此后面再加一个 intern() 方法让 String 去常量池找而不是重新 new 一个 String 对象
+        synchronized (userId.toString().intern()) {
+            // 获取代理对象（事务）
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }
+    }
+
+    @Transactional
+    public Result createVoucherOrder(Long voucherId) {
+        // 1.一人一单
+        Long userId = UserHolder.getUser().getId();
+        // 1.1.查询订单
+        int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        // 1.2.判断是否存在
+        if (count > 0) {
+            return Result.fail("已经下过单了，一个人只能下单一次！");
+        }
+        // 2.创建订单
+        // 2.1.订单 id
         VoucherOrder voucherOrder = new VoucherOrder();
         long orderId = redisIdWorker.nextId("order");
         voucherOrder.setId(orderId);
-        // 6.2.用户 id
-        Long userId = UserHolder.getUser().getId();
+        // 2.2.用户 id
         voucherOrder.setUserId(userId);
-        // 6.3.代金券 id
+        // 2.3.代金券 id
         voucherOrder.setVoucherId(voucherId);
         save(voucherOrder);
-        // 7.返回订单 id
+        // 3.返回订单 id
         return Result.ok(orderId);
     }
 }
