45f82f4ccf1fe70c2ad75c04c65d09a89e89d8eb
PermabanSun <295375895@qq.com>
2022-09-21 18:39:08 +0800

    通过 redis 实现的 id 生成器

---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,53 @@
+package com.hmdp.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @BelongProject: hm-dianping
+ * @BelongPackage: com.hmdp.utils
+ * @Author: 那个小楠瓜
+ * @CreateTime: 2022-09-21 17:08
+ * @Description: redis id 生成器
+ * @Version: 1.0
+ */
+@Component
+public class RedisIdWorker {
+    // 时间戳
+    private static final long BEGIN_TIMESTAMP = 1640995200l;
+    // 序列号的位数
+    private static final int COUNT_BITS = 32;
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RedisIdWorker(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+    /**
+     *
+     * @param keyPrefix 业务前缀
+     * @return
+     */
+    public long nextId(String keyPrefix) {
+        // 1.生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
+        long timeStamp = nowSecond - BEGIN_TIMESTAMP;
+        // 2.生成序列号
+        // 2.1.获取当前日期，精确到天
+        String date = now.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+        // 3.拼接并返回
+        return timeStamp << COUNT_BITS | count;
+    }
+
+    public static void main(String[] args) {
+        LocalDateTime time = LocalDateTime.of(2022, 1, 1, 0, 0, 0);
+        long second = time.toEpochSecond(ZoneOffset.UTC);
+        System.out.println("second = " + second);
+    }
+}
